ee8dacf8e27145dd1cac85427859cc658dcda9f5
zkazm3 <164928037+zkazm3@users.noreply.github.com>
2026-09-24 17:30:47 +0000

    finished part 2 - created a data summary table

--- load_data.py
+++ load_data.py
@@ -6,12 +6,27 @@ cd = pd.read_csv("data/cell-count.csv")
 connection = sqlite3.connect("cells.db")
 connection.execute("PRAGMA foreign_keys = ON;")
 cur = connection.cursor()
+subject_data = cd[
+    ["subject", "project", "condition", "age", "sex", "treatment", "response"]
+].drop_duplicates(subset = ["subject"])
+
+sample_data = cd[
+    ["sample", "sample_type", "subject", "time_from_treatment_start"]
+].drop_duplicates(subset = ["sample"])
+
+
+
 cell_columns = [
     "b_cell", "cd8_t_cell","cd4_t_cell", "nk_cell", "monocyte"
 ]
 
-cell_test = pd.melt(cd, id_vars=["sample"], value_vars=cell_columns, var_name = "cell_type", value_name = "cell_count")
+cell_data = pd.melt(cd, id_vars=["sample"], value_vars=cell_columns, 
+var_name = "cell_type", value_name = "cell_count")
 
+
+cur.execute("DROP TABLE IF EXISTS cell_counts")
+cur.execute("DROP TABLE IF EXISTS samples")
+cur.execute("DROP TABLE IF EXISTS subjects")
 cur.execute(""" CREATE TABLE IF NOT EXISTS subjects (
                 subject TEXT PRIMARY KEY,
                 project TEXT NOT NULL,
@@ -21,6 +36,8 @@ cur.execute(""" CREATE TABLE IF NOT EXISTS subjects (
                 treatment TEXT NOT NULL,
                 response TEXT 
             );""")
+
+
 
 cur.execute(""" CREATE TABLE IF NOT EXISTS samples (
                 sample TEXT PRIMARY KEY,
@@ -33,6 +50,7 @@ cur.execute(""" CREATE TABLE IF NOT EXISTS samples (
 
             );""")
 
+
 cur.execute(""" CREATE TABLE IF NOT EXISTS cell_counts (
                 cell_type TEXT NOT NULL,
                 sample TEXT NOT NULL,
@@ -44,8 +62,55 @@ cur.execute(""" CREATE TABLE IF NOT EXISTS cell_counts (
                     REFERENCES samples(sample)
                 
             );""")
+
+
+
+subject_data.to_sql(
+    "subjects",
+    connection,
+    if_exists="append",
+    index=False
+)
+
+sample_data.to_sql(
+    "samples",
+    connection,
+    if_exists="append",
+    index=False
+)
+
+cell_data.to_sql(
+    "cell_counts",
+    connection,
+    if_exists="append",
+    index=False
+)
+
+
+
+
 connection.commit()
 connection.close()
 
+
+
 #Part 2
+
+connection = sqlite3.connect("cells.db")
+cur = connection.cursor()
+connection.execute("PRAGMA foreign_keys = ON;")
+
+p2query = """
+    SELECT
+        sample,
+        SUM(cell_count) OVER(PARTITION BY sample) AS total_count,
+        cell_type AS population,
+        cell_count AS count,
+        ROUND((cell_count * 100.0) / SUM(cell_count) OVER(PARTITION BY sample), 2) AS percentage 
+        FROM cell_counts
+        ORDER BY sample"""
+
+summary_df = pd.read_sql_query(p2query, connection)
+
+print(summary_df.head(10))
 
